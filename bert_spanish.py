19fffbc01e26a09cdc724f7f6e1e81c34c7cbfaf
Juan Carlos Solórzano <jcarlossolc@gmail.com>
2023-12-12 10:56:45 -0600

    Corrected text parsing function

--- bert_spanish.py
+++ bert_spanish.py
@@ -1,6 +1,8 @@
 # Importar
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 from textwrap import wrap
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 #Variables para el modelo
 the_model = 'mrm8488/distill-bert-base-spanish-wwm-cased-finetuned-spa-squad2-es'
@@ -15,14 +17,17 @@ def limpiarTexto(ruta_archivo):
         # Leer el texto del archivo
         text = archivo.read()
 
-    # Reemplazar comillas dobles con sencillas
+    # Replace double quotes with single quotes
     text = text.replace('"', "'")
     
     # Dividir el texto en lineas
-    lineas = text.split('\n')
+    lines = text.split('\n')
     
-    # Unir las lineas con \n
-    textoLimpio = "\\n".join(lineas)
+    # Quitar lineas vacias
+    lines = [line for line in lines if line.strip() != '']
+    
+    # Unir las lineas
+    textoLimpio = "\\n".join(lines)
     
     return textoLimpio
 
